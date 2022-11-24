0aa713a7e74edac3988a6c7467d1619c91c533f2
yanpinar <oemer.can.yanpinar@hs-furtwangen.de>
2022-11-24 14:55:29 +0100

    Output in Main.java file fixed

--- src/main/java/de/hfu/Main.java
+++ src/main/java/de/hfu/Main.java
@@ -1,14 +1,30 @@
 package de.hfu;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        new Main();
+    }
+
+    public Main() {
+        String s = this.readInput(System.in);
         System.out.println(s.toUpperCase());
+    }
+
+    /**
+     * Reads the input string from a inputStream
+     * @param inputStream for example System.in
+     * @return typed string of the inputstream
+     */
+    public String readInput(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream);
+        String s = scanner.nextLine();
         scanner.close();
+        return s;
     }
 
 }
